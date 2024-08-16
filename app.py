3fa8c161061706b4cc3773030f78ac83684dbe4b
bothugux <huguxnet@gmail.com>
2024-08-16 01:25:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if st.button('Predecir Precio'):
     input_scaled = scaler.fit_transform(input_data)
 
     # Realizar predicción
-    prediction = modelo.predict(input_data )
+    prediction = modelo.predict( input_scaled)
 
     # Mostrar predicción
     st.write(f'Precio predecido: {prediction[0]:.2f} euros')
